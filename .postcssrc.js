f23b88a2e4627fafbf136147199f7429a7fae8b1
yangfei <yangfei@fuzhi.ai>
2022-10-14 05:16:22 +0800

    添加vue.config.js文件内容

--- .postcssrc.js
+++ .postcssrc.js
@@ -4,7 +4,7 @@ module.exports = {
       browsers: ["Android >= 4.0", "iOS >= 7"],
     },
     "postcss-pxtorem": {
-      rootValue: 54,
+      rootValue: 54, // 75表示750设计稿，54表示540设计稿
       propList: ["*"],
     },
   },
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,31 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    extract: true,
+  },
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === "production") {
+      config.mode = "production";
+      let optimization = {
+        runtimeChunk: "single",
+        splitChunks: {
+          chunks: "all",
+          cacheGroups: {
+            vendor: {
+              test: /[/]node_modules[/]/,
+              name(module) {
+                const packageName = module.context.includes("node_modules")
+                  ? module.context.match(/\/node_modules\/(.*?)(\/|$)/)[1]
+                  : module.context.split("/")[-1];
+                console.log("packageName: ", packageName, module.context);
+                return `npm/${packageName}`;
+              },
+            },
+          },
+        },
+      };
+      Object.assign(config, { optimization });
+    }
+  },
 });
